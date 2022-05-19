dabf38ac3888805c81d95d98de2a52eb6a210d38
MHinderer <hinderer98@gmail.com>
2022-05-19 18:37:53 +0200

    Aufgabe4_1

--- Test_File.js
+++ Test_File.js
@@ -1,13 +1,43 @@
 "use strict";
-window.addEventListener("load", handleLoad);
-function handleLoad(_event) {
-    let canvas = (document.querySelector(".canvas"));
-    let crc2 = (canvas.getContext("2d"));
-    crc2.fillStyle = "#FF0000";
-    crc2.fillRect(0, 0, 200, 200);
-    crc2.beginPath();
-    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
-    crc2.closePath();
-    crc2.stroke();
-}
+var Beachday;
+(function (Beachday) {
+    window.addEventListener("load", handleLoad);
+    let canvas;
+    let crc2;
+    /* interface Vector {
+      x: number;
+      y: number;
+    } */
+    function handleLoad(_event) {
+        canvas = document.querySelector("canvas");
+        crc2 = canvas.getContext("2d");
+        drawSky();
+        /* drawSun();
+        drawSea();
+        drawBeach();
+        drawCloud();
+        drawPerson();
+        drawGull(); */
+    }
+    function drawSky() {
+        crc2.rect(0, 0, canvas.width, canvas.height);
+        let gradient = crc2.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(0.63, "white");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, canvas.width, canvas.height);
+        crc2.restore();
+    }
+    /* function drawSun(_position: Vector, _size: Vector): void {}
+  
+    function drawSea(_position: Vector, _size: Vector): void {}
+  
+    function drawBeach(_position: Vector, _size: Vector): void {}
+  
+    function drawCloud(_position: Vector, _size: Vector): void {}
+  
+    function drawPerson(_position: Vector, _size: Vector): void {}
+  
+    function drawGull(_position: Vector, _size: Vector): void {} */
+})(Beachday || (Beachday = {}));
 //# sourceMappingURL=Test_File.js.map
--- Test_File.ts
+++ Test_File.ts
@@ -1,17 +1,51 @@
-window.addEventListener("load", handleLoad);
+namespace Beachday {
+  window.addEventListener("load", handleLoad);
+  let canvas: HTMLCanvasElement;
+  let crc2: CanvasRenderingContext2D;
+  /* interface Vector {
+    x: number;
+    y: number;
+  } */
 
-function handleLoad(_event: Event): void {
-  let canvas: HTMLCanvasElement = <HTMLCanvasElement>(
-    document.querySelector(".canvas")
-  );
-  let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D>(
-    canvas.getContext("2d")
-  );
-  crc2.fillStyle = "#FF0000";
-  crc2.fillRect(0, 0, 200, 200);
+  function handleLoad(_event: Event): void {
+    canvas = document.querySelector("canvas")!;
+    crc2 = canvas.getContext("2d")!;
+    drawSky();
+    /* drawSun();
+    drawSea();
+    drawBeach();
+    drawCloud();
+    drawPerson();
+    drawGull(); */
+  }
 
-  crc2.beginPath();
-  crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
-  crc2.closePath();
-  crc2.stroke();
+  function drawSky(): void {
+    crc2.rect(0, 0, canvas.width, canvas.height);
+
+    let gradient: CanvasGradient = crc2.createLinearGradient(
+      0,
+      0,
+      0,
+      canvas.height
+    );
+
+    gradient.addColorStop(0, "lightblue");
+    gradient.addColorStop(0.63, "white");
+
+    crc2.fillStyle = gradient;
+    crc2.fillRect(0, 0, canvas.width, canvas.height);
+    crc2.restore();
+  }
+
+  /* function drawSun(_position: Vector, _size: Vector): void {}
+
+  function drawSea(_position: Vector, _size: Vector): void {}
+
+  function drawBeach(_position: Vector, _size: Vector): void {}
+
+  function drawCloud(_position: Vector, _size: Vector): void {}
+
+  function drawPerson(_position: Vector, _size: Vector): void {}
+
+  function drawGull(_position: Vector, _size: Vector): void {} */
 }
